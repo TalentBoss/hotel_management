dd96354e771a1291f8e481ea78e0f8eb0550882e
TalentBoss <workmail1candy@gmail.com>
2023-10-13 05:18:21 -0700

    skeleton
    basic skeleton with theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React, { Component, Suspense } from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import './scss/style.scss'
+
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <HashRouter>
+        <Suspense fallback={loading}>
+          <Routes>
+           
+            <Route path="*" name="Home" element={<DefaultLayout />} />
+          </Routes>
+        </Suspense>
+      </HashRouter>
   );
 }
 
